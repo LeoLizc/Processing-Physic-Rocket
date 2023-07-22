27ca9c10782a64c79518ece0cc68989bb27f53d8
LeoLizc <ldlizcano@uninorte.edu.co>
2023-07-21 23:21:42 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+    public void settings(){
+        size(600,600, P3D);
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
+
+    public static void main(String[] args){
+        PApplet.main("Main");
+    }
+}
