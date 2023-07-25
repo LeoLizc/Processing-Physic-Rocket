8b457c25abb2992fa32ce10179d3ecd8bcb52045
LeoLizc <ldlizcano@uninorte.edu.co>
2023-07-25 17:45:27 -0500

    RigidBody updated with translational physics.

--- src/RigidBody.java
+++ src/RigidBody.java
@@ -1,2 +1,32 @@
-public class RigidBody {
+import processing.core.PVector;
+
+public abstract class RigidBody {
+
+    protected float mass;
+    public PVector position;
+    private PVector velocity;
+    private PVector acceleration;
+    private PVector cumulativeForce;
+
+    public RigidBody(float mass, PVector position, PVector velocity, PVector acceleration){
+        this.mass = mass;
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+        this.cumulativeForce = new PVector(0,0,0);
+    }
+
+    public void applyForce(PVector force){
+        this.cumulativeForce.add(force);
+    }
+
+    public void updatePhysics(){
+        this.acceleration = PVector.div(this.cumulativeForce, this.mass);
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.cumulativeForce = new PVector(0,0,0);
+    }
+
+
+
 }
