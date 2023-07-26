8a5e1fe3079eab1bf428b1d2de37fef95d4b0521
LeoLizc <ldlizcano@uninorte.edu.co>
2023-07-25 22:01:45 -0500

    RigidBody updated

--- src/com/leolizc/rocketSimulator/RigidBody.java
+++ src/com/leolizc/rocketSimulator/RigidBody.java
@@ -2,32 +2,31 @@ package com.leolizc.rocketSimulator;
 
 import processing.core.PVector;
 
-public abstract class RigidBody extends WorldEntity{
+public abstract class RigidBody extends WorldEntity {
 
     protected float mass;
-    private PVector velocity;
+    private final PVector velocity;
     private PVector acceleration;
     private PVector cumulativeForce;
 
-    public RigidBody(float mass, PVector position, PVector velocity, PVector acceleration){
-        super(position);
+    public RigidBody(Simulator p, float mass, PVector position, PVector velocity, PVector acceleration) {
+        super(p, position);
         this.mass = mass;
         this.velocity = velocity;
         this.acceleration = acceleration;
-        this.cumulativeForce = new PVector(0,0,0);
+        this.cumulativeForce = new PVector(0, 0, 0);
     }
 
-    public void applyForce(PVector force){
+    public void applyForce(PVector force) {
         this.cumulativeForce.add(force);
     }
 
-    public void updatePhysics(){
+    public void updatePhysics() {
         this.acceleration = PVector.div(this.cumulativeForce, this.mass);
         this.velocity.add(this.acceleration);
         this.position.add(this.velocity);
-        this.cumulativeForce = new PVector(0,0,0);
+        this.cumulativeForce = new PVector(0, 0, 0);
     }
-
 
 
 }
