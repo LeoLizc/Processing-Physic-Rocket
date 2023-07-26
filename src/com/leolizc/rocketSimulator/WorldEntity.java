11fbd59f214fb1bc04e8193ecc665d69b6155c6f
LeoLizc <ldlizcano@uninorte.edu.co>
2023-07-25 22:01:19 -0500

    WorldEntity now references Simulator

--- src/com/leolizc/rocketSimulator/WorldEntity.java
+++ src/com/leolizc/rocketSimulator/WorldEntity.java
@@ -4,18 +4,29 @@ import processing.core.PVector;
 
 public abstract class WorldEntity {
 
+    public Simulator p;
     public PVector position;
     public PVector scale;
 
-    public WorldEntity(PVector position){
+    public WorldEntity(Simulator p, PVector position) {
+        this.p = p;
+
         this.position = position;
-        this.scale = new PVector(1,1,1);
+        this.scale = new PVector(1, 1, 1);
     }
 
-    public abstract void draw();
+    public WorldEntity(Simulator p) {
+        this.p = p;
+
+        this.position = new PVector(0, 0, 0);
+        this.scale = new PVector(1, 1, 1);
+    }
+
+    public abstract void render();
+
     public abstract void update();
 
-    public void setScale(float x, float y, float z){
-        this.scale = new PVector(x,y,z);
+    public void setScale(float x, float y, float z) {
+        this.scale = new PVector(x, y, z);
     }
 }
