93869a161a6070414f073481a2424b174e5c111f
LeoLizc <leolizcanop@gmail.com>
2024-03-29 19:33:55 -0500

    Rocket now draws as a cylinder

--- src/com/leolizc/rocketSimulator/Rocket.java
+++ src/com/leolizc/rocketSimulator/Rocket.java
@@ -1,5 +1,7 @@
 package com.leolizc.rocketSimulator;
 
+import processing.core.PApplet;
+import processing.core.PShape;
 import processing.core.PVector;
 
 public class Rocket extends RigidBody{
@@ -7,9 +9,48 @@ public class Rocket extends RigidBody{
         super(p, mass, position, velocity, acceleration);
     }
 
+    public Rocket(Simulator p){
+        super(p, 10, new PVector(0, -200, -200));
+    }
+
     @Override
     protected void drawObject() {
+        int numOfFaces=50;
+        float h = 100, r = 8;
 
+        p.stroke(100, 100, 100);
+        p.fill(200f);
+
+        this.applyForce(new PVector(0, -9.8f/12, 0));
+
+        // Top circle
+        p.beginShape();
+//        p.stroke(0 ,0, 255);
+//        p.fill(200f);
+        for (int i = 0; i < numOfFaces; i++) {
+            float angle = PApplet.map(i, 0, numOfFaces, 0, p.TWO_PI);
+            p.vertex(r * PApplet.cos(angle), -h / 2f, r * PApplet.sin(angle));
+        }
+        p.endShape(p.CLOSE);
+
+        //Cylindrical body
+        p.beginShape(p.QUAD_STRIP);
+        for (int i = 0; i <= numOfFaces; i++) {
+            float angle = PApplet.map(i, 0, numOfFaces, 0, p.TWO_PI);
+
+            p.vertex(r * PApplet.cos(angle), -h / 2f, r * PApplet.sin(angle));
+            p.vertex(r * PApplet.cos(angle), h / 2f, r * PApplet.sin(angle));
+        }
+        p.endShape();
+
+        // Bottom circle
+        p.beginShape(p.TRIANGLE_FAN);
+        p.vertex(0, h / 2f, 0);
+        for (int i = 0; i <= numOfFaces; i++) {
+            float angle = PApplet.map(i, 0, numOfFaces, 0, p.TWO_PI);
+            p.vertex(r * PApplet.cos(angle), h / 2f, r * PApplet.sin(angle));
+        }
+        p.endShape();
     }
 
     @Override
