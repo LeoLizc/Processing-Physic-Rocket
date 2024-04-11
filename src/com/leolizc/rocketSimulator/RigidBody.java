b2f3de0b323e6ad4fd3a054afb14ce1f256ff692
LeoLizc <leolizcanop@gmail.com>
2024-04-10 21:56:41 -0500

    RigidBody.java refactor

--- src/com/leolizc/rocketSimulator/RigidBody.java
+++ src/com/leolizc/rocketSimulator/RigidBody.java
@@ -7,14 +7,14 @@ import processing.core.PVector;
 public abstract class RigidBody extends WorldEntity {
 
     private final PVector velocity;
-    protected float mass;
     private final PVector acceleration;
-    private PVector cumulativeForce;
-    public PVector angularVelocity;
     private final PVector cumulativeTorque;
+    public PVector angularVelocity;
     public PMatrix3D iInertiaTensor;
     public boolean blockLinearMovement = false;
     public boolean blockRotation = false;
+    protected float mass;
+    private PVector cumulativeForce;
 
     public RigidBody(
             Simulator p, float mass, PMatrix3D iInertiaTensor,
@@ -80,24 +80,24 @@ public abstract class RigidBody extends WorldEntity {
 
         PVector acceleration = PVector.div(this.cumulativeForce, this.mass).add(this.acceleration);
         this.velocity.add(acceleration);
-        if(!this.blockLinearMovement)
-            this.position.add(PVector.mult(this.velocity, p.deltaTime*0.4f));
+        if (!this.blockLinearMovement)
+            this.position.add(PVector.mult(this.velocity, p.deltaTime * 0.4f));
         this.cumulativeForce = new PVector(0, 0, 0);
 //        System.out.println("LINEAR MOVEMENT:");
 //        System.out.println("Position: "+this.position+"\nVelocity: "+this.velocity+"\nAcceleration: "+acceleration);
 
 //        Angular Acceleration = I^-1 * T
-        PVector angAcc = new PVector(0,0,0);
+        PVector angAcc = new PVector(0, 0, 0);
         angAcc = this.iInertiaTensor.mult(this.cumulativeTorque, angAcc);
         this.angularVelocity.add(angAcc);
-        if(!this.blockRotation)
+        if (!this.blockRotation)
             this.rotateByAxis(
-                this.angularVelocity.mag()*p.deltaTime,
-                this.angularVelocity
-        );
+                    this.angularVelocity.mag() * p.deltaTime,
+                    this.angularVelocity
+            );
         System.out.println("ANGULAR MOVEMENT:");
-        System.out.println("Torque: "+this.cumulativeTorque);
-        System.out.println("Angular Velocity: "+this.angularVelocity+"\nAngular Acceleration: "+angAcc+"\nRotation: "+this.rotation);
+        System.out.println("Torque: " + this.cumulativeTorque);
+        System.out.println("Angular Velocity: " + this.angularVelocity + "\nAngular Acceleration: " + angAcc + "\nRotation: " + this.rotation);
         this.cumulativeTorque.set(0, 0, 0);
 
     }
